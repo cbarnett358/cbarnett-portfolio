66082dc4c3fa0a4014e0dda14fc01bb7507a08d1
Chris Barnett <cbarnett2813@gmail.com>
2023-01-30 00:14:45 -0600

    Added titles to projects and fixed colums for different screen sizes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <div class="container q-mx-md q-mb-none">
+    <div class="text-h2 text-secondary">VIEW PROJECTS</div>
+  </div>
+
+  <q-page class="container q-mx-sm q-mb-none">
+    <PortfolioCards></PortfolioCards>
   </q-page>
 </template>
 
 <script>
+import PortfolioCards from "src/components/PortfolioCards.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+  components: { PortfolioCards },
 });
 </script>
